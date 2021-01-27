ab054d22b8732c15873eb6f12ed655a2cd985b8c
Md Miyad Hossain <miyad.mh@gmail.com>
2021-01-27 13:53:05 +0600

    mobile and cover items and price done

--- script.js
+++ script.js
@@ -1,27 +1,73 @@
-// plus icon click number increase
+function increaseNumber (id) {
+    const itemNumber = document.getElementById(id).value;
+    const itemNumberInt = parseInt(itemNumber);
+    const total = itemNumberInt + 1;
+    document.getElementById(id).value = total;
+    return total;
+}
+
+function decreaseNumber (id) {
+    const itemNumber = document.getElementById(id).value;
+    const itemNumberInt = parseInt(itemNumber);
+    const total = itemNumberInt - 1;
+    document.getElementById(id).value = total;
+    return total;
+}
+
+function phonePrice (price,items,id) {
+    const totalPrice = price * items;
+    document.getElementById(id).innerText = totalPrice;
+}
+
+
+
+// plus icon click number increase for mobile
 const plusIcon = document.getElementById('plus-icon');
 plusIcon.addEventListener('click', function() {
     
-    const itemNumber = document.getElementById('item-number').value;
-    const pargeInt = parseInt(itemNumber);
-    const total = pargeInt + 1;
-    document.getElementById('item-number').value = total;
+    // increase number
+    const itemNumIncrease = increaseNumber('item-number');
 
-    // const priceText = document.getElementById('price1').innerText;
-    // const pargeInt = parseInt(priceText);
-    // const result = pargeInt * numberTotal;
-    // document.getElementById('price1').innerText = result;
+    // phone price with increase
+    phonePrice(1219, itemNumIncrease, 'price1');
+    
 })
 
-// minus icon click number decrease
+// minus icon click number decrease for mobile
 const minusIcon = document.getElementById('minus-icon');
 minusIcon.addEventListener('click', function() {
     
-    const itemNumber = document.getElementById('item-number').value;
-    const pargeInt = parseInt(itemNumber);
-    const total = pargeInt - 1;
-    document.getElementById('item-number').value = total;
+    // decrease number
+    const itemNumDecrease =  decreaseNumber('item-number');
+
+    // phone price with decrease
+    phonePrice(1219, itemNumDecrease, 'price1');
+    
 })
 
 
 
+// plus icon click number increase for cover
+const plusIcon2 = document.getElementById('plus-icon2');
+plusIcon2.addEventListener('click', function() {
+    
+    // increase number
+    const itemNum2Increase = increaseNumber('item-number2');
+
+    // phone price with increase
+    phonePrice(59, itemNum2Increase, 'price2');
+    
+})
+
+
+// minus icon click number decrease for cover
+const minusIcon2 = document.getElementById('minus-icon2');
+minusIcon2.addEventListener('click', function() {
+    
+    // decrease number
+    const itemNum2Decrease =  decreaseNumber('item-number2');
+
+    // phone price with decrease
+    phonePrice(59, itemNum2Decrease, 'price2');
+    
+})
